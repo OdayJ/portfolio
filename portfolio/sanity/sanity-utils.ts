1b78cb91cc5553c388bda33331e702e20ab94f27
Oday Jawaada <Jawaada.personal@gmail.com>
2023-11-15 19:15:36 +0000

    Bug fixes

--- portfolio/sanity/sanity-utils.ts
+++ portfolio/sanity/sanity-utils.ts
@@ -2,8 +2,8 @@ import { createClient, groq } from "next-sanity";
 
 // Reuse the same client for both functions
 const client = createClient({
-  projectId: "34mi31wy",
-  dataset: "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2023-11-13",
   useCdn: false,
 });
